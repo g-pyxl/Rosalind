95e61ff1f3eef30ba8568923c6c123fc3778a91d
George <64019054+g-pyxl@users.noreply.github.com>
2020-04-25 22:03:15 +0100

    Create IEV - Calculating Expected Offspring.py

---
+++ IEV - Calculating Expected Offspring.py
@@ -0,0 +1,11 @@
+def offcalc(a, b, c, d, e, f):
+    a1 = a * 2
+    b1 = b * 2
+    c1 = c * 2
+    d1 = (d * 2) * 0.75
+    e1 = (e * 2) * 0.50
+    f1 = f * 0
+
+    return a1 + b1 + c1 + d1 + e1 + f1
+
+print (offcalc(1, 0, 0, 1, 0, 1))
